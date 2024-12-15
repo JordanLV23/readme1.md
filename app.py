c910895e713a05f2799d09863780d42a4c2877b1
JordanLV23 <133944863+JordanLV23@users.noreply.github.com>
2024-12-14 20:50:43 -0600

    app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+car_data = pd.read_csv('c:\\Users\\jlina\\Downloads\\vehicles_us.csv')
+hist_button = st.button('Construir Histograma')
+
+if hist_button:
+    st.write('Creación del Histograma para el conjunto de datos de anuncios de venta de coches')
+    fig=px.histogram(car_data, x='odometer')
+    st.plotly_chart(fig,use_container_width=True)
+
+
+disp_button = st.button('Construir Gráfico de Dispersión')
+
+if disp_button:
+    st.write('Creación del gráfico de disperción para el conjunto de datos de anuncios de venta de coches')
+    fig=px.histogram(car_data, x='odometer',y='price')
+    st.plotly_chart(fig,use_container_width=True)
