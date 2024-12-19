febbeac15c894b431e4c5ab583b2b77a7d477679
JordanLV23 <133944863+JordanLV23@users.noreply.github.com>
2024-12-18 19:37:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -18,4 +18,5 @@ disp_button = st.button('Construir Gráfico de Dispersión')
 if disp_button:
     st.write('Creación del gráfico de disperción para el conjunto de datos de anuncios de venta de coches')
     fig=px.scatter(car_data, x='odometer',y='price')
-    st.plotly_chart(fig,use_container_width=True)
+    #st.plotly_chart(fig,use_container_width=True)
+    fig.show() 
